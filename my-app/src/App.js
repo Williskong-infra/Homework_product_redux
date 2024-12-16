df529f87eec18053794c58c8c40fd3b3e0b592bd
Willis Kong <willis.kong@gmail.com>
2024-12-16 16:46:25 +0800

    add RootLayout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 import Product from './components/Product';
-import {createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Cart from './components/Cart';
+import RootLayout from './components/RootLayout';
 
 
 
 function App() {
 
-  const route = createBrowserRouter(createRoutesFromElements(
-    <Route>
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
       <Route index element={<Dashboard />}></Route>
       <Route path="/cart" element={<Cart />}></Route>
     </Route>
--- my-app/src/components/RootLayout.js
+++ my-app/src/components/RootLayout.js
@@ -1,12 +1,16 @@
+import { Outlet } from "react-router-dom";
+
 
 const RootLayout = () => {
     return(
         <div>
             <h1>Navigation</h1>
+
+        <main>
+            <Outlet />
+        </main>
         </div>
-
     )
-
 }
 
 export default RootLayout;
